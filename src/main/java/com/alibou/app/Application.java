c463cf3bbd8786cbc0a1231b3eac297cf8e75b9b
pedro <pedro.jcaceresl@gmail.com>
2026-02-13 20:41:47 -0300

    Add CommandLineRunner to initialize ROLE_USER in the database

--- src/main/java/com/alibou/app/Application.java
+++ src/main/java/com/alibou/app/Application.java
@@ -1,13 +1,31 @@
 package com.alibou.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.alibou.app.role.Role;
+import com.alibou.app.role.RoleRepository;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("ROLE_USER").isEmpty()) {
+                roleRepository.save(
+                        Role.builder()
+                                .name("ROLE_USER")
+                                .build()
+                );
+            }
+        };
     }
 
 }
